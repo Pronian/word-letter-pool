ee661b64f5fb8d7d7a0a22fe456f04a55315cd4d
Ivan Georgiev <ivan.georgiev4360@gmail.com>
2022-07-11 06:14:45 +0300

    Fixed dictionary path to work outside cwd

--- dictionary.ts
+++ dictionary.ts
@@ -3,8 +3,10 @@
  * @returns - An array of words.
  */
 export function loadWords() {
+  const dirname = new URL('.', import.meta.url).pathname;
+
   // Use dictionary from https://github.com/dwyl/english-words
-  const allWords = Deno.readTextFileSync("words_alpha.txt")
+  const allWords = Deno.readTextFileSync(`${dirname}/words_alpha.txt`)
     .split("\n")
     .map((word) => word.trim());
 
